9f83516fca60ed54b65cfd3ea6cddeb5cd6d15f7
Addison Higham <ahigham@instructure.com>
2015-05-01 10:29:01 -0600

    Dynamic LTI provider based on req

--- test/strategy.normal.test.js
+++ test/strategy.normal.test.js
@@ -46,4 +46,53 @@ describe('Strategy', function() {
       });
     });
   });
+  describe('handle dynamic oauth secrets', function() {
+    // example createProvider, you might end up looking instituion, etc to find the shared secrets
+    function createProvider(req, cb) {
+      cb(null, new lti.Provider(req.body.consumerKey, req.body.consumerSecret));
+    }
+
+
+    var strategy = new Strategy({createProvider: createProvider}, function(lti, done) {
+      return done(null, {
+        id: lti.user_id
+      }, {
+        scope: 'read'
+      });
+    });
+
+    var user;
+    var info;
+    before(function(done) {
+      chai.passport.use(strategy)
+      .success(function(u, i) {
+        user = u;
+        info = i;
+        done();
+      })
+      .req(function(req) {
+        req.body = CONFIG.body();
+        req.body.consumerSecret = CONFIG.lti.consumerSecret;
+        req.body.consumerKey = CONFIG.lti.consumerKey;
+        req.get = function() {
+          return 'test-get';
+        };
+        var provider = new lti.Provider(req.body.consumerKey, req.body.consumerSecret);
+        req.body.oauth_signature = provider.signer.build_signature(req, CONFIG.lti.consumerSecret);
+      })
+      .authenticate();
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info).to.deep.equal({
+        scope: 'read'
+      });
+    });
+  });
 });
